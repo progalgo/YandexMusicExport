5a47d49c9c1624a8743621ed9cd5c184c2fa1a86
Roman Ustinov <romanustinov9@gmail.com>
2024-06-16 00:41:04 +0300

    Refactor

---
+++ YandexMusicExport/Model.cs
@@ -0,0 +1,42 @@
+﻿namespace YandexMusicExport;
+internal class PlaylistResponse
+{
+    public required Playlist Playlist { get; set; }
+}
+
+internal class Playlist
+{
+    public required string Title { get; set; }
+    public required Track[] Tracks { get; set; }
+}
+
+static class PlaylistExtensions
+{
+    public static IEnumerable<string> GetPlaylistLines(this Playlist playlist)
+    {
+        foreach (Track track in playlist.Tracks)
+        {
+            yield return track.GetTextRecord();
+        }
+    }
+}
+
+internal class Track
+{
+    public required string Title { get; set; }
+    public required Artist[] Artists { get; set; }
+}
+
+static class TrackExtensions
+{
+    public static string GetTextRecord(this Track track)
+    {
+        var artistsNames = string.Join(", ", track.Artists.Select(x => x.Name));
+        return artistsNames + " - " + track.Title;
+    }
+}
+
+internal class Artist
+{
+    public required string Name { get; set; }
+}
--- YandexMusicExport/Program.cs
+++ YandexMusicExport/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System.Diagnostics;
 using System.Net;
-using System.Text;
 using System.Text.Json;
 
 namespace YandexMusicExport;
@@ -29,88 +28,33 @@ internal static class Program
         var owner = inputUri.Segments[2].Trim('/');
         var kinds = inputUri.Segments[4].Trim('/');
 
-        var uri = "https://music.yandex.ru/handlers/playlist.jsx?owner=" + owner + "&kinds=" + kinds;
+        var uri = $"https://music.yandex.ru/handlers/playlist.jsx?owner={owner}&kinds={kinds}";
 
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Обработка...\n");
+        Console.WriteLine("Обработка...");
         Console.ResetColor();
 
-        try
+        var client = new WebClient();
+        var response = JsonSerializer.Deserialize<PlaylistResponse>(client.OpenRead(uri), new JsonSerializerOptions { PropertyNameCaseInsensitive = true })!;
+
+        Playlist playlist = response.Playlist;
+
+        using (TextWriter writer = new StreamWriter($"{playlist.Title}.txt"))
         {
-            var client = new WebClient();
-            var response = JsonSerializer.Deserialize<PlaylistResponse>(client.OpenRead(uri), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-
-            var playlistTitle = response?.Playlist.Title;
-            var tracks = response?.Playlist.Tracks;
-
-            StringBuilder allFile = new();
-
-            foreach (var track in tracks!)
+            foreach (string line in playlist.GetPlaylistLines())
             {
-                var artistsNames = "";
-
-                foreach (var artist in track.Artists) artistsNames += artist.Name + ", ";
-
-                artistsNames = artistsNames.TrimEnd(',', ' ');
-
-                var fullTrack = artistsNames + " - " + track.Title + "\n";
-
-                allFile.Append(fullTrack);
+                writer.WriteLine(line);
             }
-
-            File.WriteAllText($"{playlistTitle}.txt", allFile.ToString());
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Готово!\n");
-            Console.ResetColor();
-
-            Console.WriteLine($"Название плейлиста: {playlistTitle}\n" +
-                              $"Список треков распечатан ниже и сохранен рядом с файлом программы (файл {playlistTitle}.txt).\n");
-
-            Process.Start(new ProcessStartInfo($"{playlistTitle}.txt") { UseShellExecute = true });
-
-        }
-        catch (JsonException e)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Ошибка! Несуществующий плейлист или временный бан от Яндекса. Проверьте ссылку " +
-                              "и попробуйте еще раз через некоторое время или на другом устройстве. Также есть сайт " +
-                              "https://files.u-pov.ru/programs/YandexMusicExport" +
-                              ", но велика вероятность, что там " +
-                              "вообще ничего не будет работать :) \n" +
-                              "Если вообще ничего не помогает, напишите мне https://t.me/aleqsanbr.");
-            Console.ResetColor();
-            Console.WriteLine("\nДополнительная информация:");
-            Console.WriteLine(e);
         }
 
-        catch (IndexOutOfRangeException e)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Ошибка! Вероятно, некорректная ссылка. Проверьте, чтобы она была вида " +
-                              "https://music.yandex.ru/users/USERNAME/playlists/PLAYLIST_ID, т. е. без лишних " +
-                              "параметров. Попробуйте еще раз. Если ничего не работает, напишите мне https://t.me/aleqsanbr.");
-            Console.ResetColor();
-            Console.WriteLine("\nДополнительная информация:");
-            Console.WriteLine(e);
-        }
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Готово!");
+        Console.ResetColor();
 
-        catch (Exception e)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Ошибка! Проверьте правильность ссылки и попробуйте еще раз. " +
-                              "Также учтите, что из-за большого количества запросов может последовать временный " +
-                              "бан от Яндекса. В таком случае попробуйте с другого устройства или на сайте " +
-                              "https://files.u-pov.ru/programs/YandexMusicExport" + ". " +
-                              "Если ничего не работает, напишите мне https://t.me/aleqsanbr.");
-            Console.ResetColor();
-            Console.WriteLine("\nДополнительная информация:");
-            Console.WriteLine(e);
+        Console.WriteLine($"Название плейлиста: {playlist.Title}\n");
+        Console.WriteLine($"Список треков распечатан ниже и сохранен рядом с файлом программы (файл {playlist.Title}.txt).");
 
-        }
-
-        Console.WriteLine("\nДля закрытия нажмите любую клавишу...");
-        Console.ReadKey();
+        Process.Start(new ProcessStartInfo($"{playlist.Title}.txt") { UseShellExecute = true });
     }
 
     private static void Greet()
@@ -141,26 +85,4 @@ internal static class Program
                           "ссылка, подпишитесь на канал :)" +
                           "\n");
     }
-}
-
-internal class PlaylistResponse
-{
-    public required PlaylistData Playlist { get; set; }
-}
-
-internal class PlaylistData
-{
-    public required string Title { get; set; }
-    public required Track[] Tracks { get; set; }
-}
-
-internal class Track
-{
-    public required string Title { get; set; }
-    public required Artist[] Artists { get; set; }
-}
-
-internal class Artist
-{
-    public required string Name { get; set; }
 }
