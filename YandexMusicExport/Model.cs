736eba0b0bc8df53915c6aeb1bdc5b704c323a50
Roman Ustinov <romanustinov9@gmail.com>
2024-06-16 11:39:42 +0300

    Separate model and extensions

--- YandexMusicExport/Model.cs
+++ YandexMusicExport/Model.cs
@@ -10,30 +10,10 @@ internal class Playlist
     public required Track[] Tracks { get; set; }
 }
 
-static class PlaylistExtensions
-{
-    public static IEnumerable<string> GetPlaylistLines(this Playlist playlist)
-    {
-        foreach (Track track in playlist.Tracks)
-        {
-            yield return track.GetTextRecord();
-        }
-    }
-}
-
 internal class Track
 {
     public required string Title { get; set; }
     public required Artist[] Artists { get; set; }
-}
-
-static class TrackExtensions
-{
-    public static string GetTextRecord(this Track track)
-    {
-        var artistsNames = string.Join(", ", track.Artists.Select(x => x.Name));
-        return artistsNames + " - " + track.Title;
-    }
 }
 
 internal class Artist
---
+++ YandexMusicExport/ModelExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace YandexMusicExport;
+
+static class PlaylistExtensions
+{
+    public static IEnumerable<string> GetPlaylistLines(this Playlist playlist)
+    {
+        foreach (Track track in playlist.Tracks)
+        {
+            yield return track.GetTextRecord();
+        }
+    }
+}
+
+static class TrackExtensions
+{
+    public static string GetTextRecord(this Track track)
+    {
+        var artistsNames = string.Join(", ", track.Artists.Select(x => x.Name));
+        return artistsNames + " - " + track.Title;
+    }
+}
